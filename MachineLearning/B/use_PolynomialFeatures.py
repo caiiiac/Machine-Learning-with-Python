7976444077195efa9174bd19a291d968fb314ce0
caiiiac <caiiiac@163.com>
2017-06-04 15:31:29 +0800

    add 多项式回归代码

---
+++ MachineLearning/B/use_PolynomialFeatures.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import linear_model
+from sklearn.preprocessing import PolynomialFeatures
+ 
+ 
+# 读取数据集
+datasets_X = []
+datasets_Y = []
+fr = open('prices.txt','r')
+lines = fr.readlines()
+for line in lines:
+    items = line.strip().split(',')
+    datasets_X.append(int(items[0]))
+    datasets_Y.append(int(items[1]))
+ 
+length = len(datasets_X)
+datasets_X = np.array(datasets_X).reshape([length,1])
+datasets_Y = np.array(datasets_Y)
+ 
+minX = min(datasets_X)
+maxX = max(datasets_X)
+X = np.arange(minX,maxX).reshape([-1,1])
+ 
+ 
+poly_reg = PolynomialFeatures(degree = 2)
+X_poly = poly_reg.fit_transform(datasets_X)
+lin_reg_2 = linear_model.LinearRegression()
+lin_reg_2.fit(X_poly, datasets_Y)
+ 
+# 图像中显示
+plt.scatter(datasets_X, datasets_Y, color = 'red')
+plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color = 'blue')
+plt.xlabel('Area')
+plt.ylabel('Price')
+plt.show()
